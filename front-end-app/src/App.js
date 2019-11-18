fa8a129ed4b85ab25e22c3935ec497094556fb13
astubbings <53932400+astubbings@users.noreply.github.com>
2019-11-18 14:17:58 -0700

    a mess of a form

--- front-end-app/src/App.js
+++ front-end-app/src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 
 import Header from './components/Header';
 import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Route exact path='/' component={Register} />
+      <Route exact path='/Register' component={Register} />
+      <Route exact path='/Login' component={Login}/>
     </div>
   );
 }
--- front-end-app/src/components/Register.js
+++ front-end-app/src/components/Register.js
@@ -1,25 +1,54 @@
 import React from "react";
 
 import { Link } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 import '../App.css';
-import logo from '../logo.svg';
 
 function RegisterPage() {
 
-    return (
-        <section >
-            <header>
-                <h2>Please Register to Start A Conversation</h2>
-                <Link to="/"><button>Go to Home</button></Link>
-                <Link to="/Login"><button>Go to Login</button></Link>
-                <br />
-                <br />
-                <img src={logo} className="App-logo" alt="logo"/>
-                
-            </header>
-        </section>
-    );
+return (
+<section >
+<div>
+    <h2>Please Register to Start A Conversation</h2>
+    <Link to="/"><button>Go to Home</button></Link>
+    <Link to="/Login"><button>Go to Login</button></Link>
+    <Formik
+      initialValues={{ username: '', phone: '', email: '', password: '' }}
+      validate={values => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field type="text" name="username" />
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" component="div" />
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" component="div" />
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+</div>
+</section>
+);
 }
 
 export default RegisterPage;
