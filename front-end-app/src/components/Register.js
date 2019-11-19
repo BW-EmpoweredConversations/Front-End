2a975c4aca4f9bec85eda46317d1e282fe1745b7
astubbings <53932400+astubbings@users.noreply.github.com>
2019-11-19 11:04:14 -0700

    final reg form before react 2 integration

--- front-end-app/src/components/Register.js
+++ front-end-app/src/components/Register.js
@@ -1,9 +1,107 @@
 import React from "react";
 
-//import { Link } from 'react-router-dom';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-
 import '../App.css';
+
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+
+
+
+
+function BaseForm({ values, errors, touched }) {
+  return (
+    <Form>
+      <div className='form-field'>
+        <label >
+          Name:&nbsp;
+              <Field type='text' name='name' />
+          {(touched.name && errors.name) ? <p className="error">{errors.name}</p> : ''}
+        </label>
+      </div>
+
+      <div className='form-field'>
+        <label >
+          Phone:&nbsp;
+              <Field type='text' name='phone' />
+          {(touched.phone && errors.phone) ? <p className="error">{errors.phone}</p> : ''}
+        </label>
+      </div>
+      
+      <div className='form-field'>
+        <label >
+          Email:&nbsp;
+              <Field type='email' name='email' />
+          {(touched.email && errors.email) ? <p className="error">{errors.email}</p> : ''}
+        </label>
+      </div>
+
+      <div className='form-field'>
+        <label >
+          Password:&nbsp;
+              <Field type='password' name='password' />
+          {(touched.password && errors.password) ? <p className="error">{errors.password}</p> : ''}
+        </label>
+      </div>
+
+      <div className='form-field'>
+        <label >
+          <Field type='checkbox' name='terms' checked={values.terms} />
+          I accept the Terms of Service
+              {(touched.terms && errors.terms) ? <p className="error">{errors.terms}</p> : ''}
+        </label>
+      </div>
+      
+      <div className='form-field'>
+        <button type='submit'>Submit</button>
+      </div>
+
+    </Form>
+  )
+};
+
+const RegFormFormik = withFormik({
+  mapPropsToValues: ({ name, phone, email, password, terms }) => {
+    return {
+      name: name || '',
+      phone: phone || '',
+      email: email || '',
+      password: password || '',
+      terms: terms || true,
+    }
+  },
+  handleSubmit: (values, { props, resetForm }) => {
+    //console.log(props)
+    axios.post('https://reqres.in/api/users', values)
+      .then(resp => {
+        //console.log(resp)
+        props.addUser(resp.data)
+        resetForm({
+          name: '',
+          phone: '',
+          email: '',
+          password: '',
+          terms: true,
+        })
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  },
+  validationSchema: Yup.object().shape({
+    name: Yup.string().min(2).required(),
+    phone: Yup.string().min(7).required(),
+    // role: Yup.mixed().required(),
+    email: Yup.string().email().required(),
+    password: Yup.string().min(4).required(),
+    terms: Yup.boolean().oneOf([true], 'Please agree to the Terms of Service'),
+  })
+});
+
+export default RegFormFormik(BaseForm);
+
+
 
 // function Register() {
 
@@ -11,7 +109,7 @@ import '../App.css';
 // <section >
 // <div>
 //     <h2>Please Register to Start A Conversation</h2>
-    
+
 //     <Formik
 //       initialValues={{ username: '', phone: '', email: '', password: '' }}
 //       validate={values => {
