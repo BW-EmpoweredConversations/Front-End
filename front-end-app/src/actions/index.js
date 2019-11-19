9b1919e889e624037b72c9eaa42552fa59d4a4ab
Brianna Workman <brianna.workman@hotmail.com>
2019-11-18 18:16:00 -0600

    Completes login page

--- front-end-app/src/actions/index.js
+++ front-end-app/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-export const SIGNUP_START = "REGISTRATION_START";
-export const SIGNUP_SUCCESS = "REGISTRATION_SUCCESS";
-export const SIGNUP_FAIL = "REGISTRATION_FAIL";
+export const SIGNUP_START = "SIGNUP_START";
+export const SIGNUP_SUCCESS = "SIGNUP_SUCCESS";
+export const SIGNUP_FAIL = "SIGNUP_FAIL";
 
 export const LOGIN_START = "LOGIN_START";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
@@ -10,7 +10,10 @@ export const LOGIN_FAIL = "LOGIN_FAIL";
 export const postRegistration = newUser => dispatch => {
   dispatch({ type: SIGNUP_START });
   axios
-    .post("/api/auth/register", newUser)
+    .post(
+      "https://empoweredconversations.herokuapp.com/api/auth/register",
+      newUser
+    )
     .then(res => dispatch({ type: SIGNUP_SUCCESS, payload: res.data }))
     .catch(err => dispatch({ type: SIGNUP_FAIL, payload: err }));
 };
@@ -18,7 +21,7 @@ export const postRegistration = newUser => dispatch => {
 export const userLogin = user => dispatch => {
   dispatch({ type: LOGIN_START });
   axios
-    .post("/api/auth/login", user)
+    .post("https://empoweredconversations.herokuapp.com/api/auth/login", user)
     .then(res => dispatch({ type: LOGIN_SUCCESS, payload: res.data }))
     .catch(err => dispatch({ type: LOGIN_FAIL, payload: err }));
 };
