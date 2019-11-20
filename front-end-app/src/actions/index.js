86683bc9f246692f89c67a315834c14a1ba5bd01
Brianna Workman <brianna.workman@hotmail.com>
2019-11-20 11:41:28 -0600

    Sets up redux for message

--- front-end-app/src/actions/index.js
+++ front-end-app/src/actions/index.js
@@ -7,6 +7,10 @@ export const LOGIN_START = "LOGIN_START";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 
+export const SEND_MESSAGE_START = "SEND_MESSAGE_START";
+export const SEND_MESSAGE_SUCCESS = "SEND_MESSAGE_SUCCESS";
+export const SEND_MESSAGE_FAIL = "SEND_MESSAGE_FAIL";
+
 export const postRegistration = newUser => dispatch => {
   dispatch({ type: SIGNUP_START });
   axios
@@ -27,4 +31,16 @@ export const userLogin = user => dispatch => {
     .post("https://empoweredconversations.herokuapp.com/api/auth/login", user)
     .then(res => dispatch({ type: LOGIN_SUCCESS, payload: res.data }))
     .catch(err => dispatch({ type: LOGIN_FAIL, payload: err }));
+};
+
+export const messageSend = newMessage => dispatch => {
+  dispatch({ type: SEND_MESSAGE_START });
+  axios
+    .post(
+      "https://empoweredconversations.herokuapp.com/api/auth/register",
+      newMessage
+    )
+    .then(res => dispatch({ type: SEND_MESSAGE_SUCCESS, payload: res.data }))
+    .then()
+    .catch(err => dispatch({ type: SEND_MESSAGE_FAIL, payload: err }));
 };
